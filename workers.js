c9b81b6a033ae0624c5c1b7a92133ad149087fea
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-04-06 17:58:02 +0200

    Added newrelic

--- workers.js
+++ workers.js
@@ -1,3 +1,4 @@
+require('newrelic')
 require('dotenv').config();
 var Raven = require('raven');
 Raven.config('https://386d9fe693df4a56b26b1a549d0372a0:f6d8e784e378493e8cf1556660b1cad6@sentry.io/711243').install();

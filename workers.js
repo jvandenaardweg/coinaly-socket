179294c64a46b0ea390231b993698c846d715067
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-04-09 14:17:20 +0200

    Wrap worker in raven context for error logging

--- workers.js
+++ workers.js
@@ -1,23 +1,29 @@
 require('newrelic')
 require('dotenv').config();
 var Raven = require('raven');
-Raven.config('https://386d9fe693df4a56b26b1a549d0372a0:f6d8e784e378493e8cf1556660b1cad6@sentry.io/711243').install();
-const Binance = require('./workers/binance')
-const Bittrex = require('./workers/bittrex')
-const moment = require('moment')
+Raven.config('https://386d9fe693df4a56b26b1a549d0372a0:f6d8e784e378493e8cf1556660b1cad6@sentry.io/711243', {
+  captureUnhandledRejections: true,
+  autoBreadcrumbs: true
+}).install();
 
-const binanceWorker = new Binance()
-binanceWorker.start()
+Raven.context(function () {
+  const Binance = require('./workers/binance')
+  const Bittrex = require('./workers/bittrex')
+  const moment = require('moment')
 
-const bittrexWorker = new Bittrex()
-bittrexWorker.start()
+  const binanceWorker = new Binance()
+  binanceWorker.start()
 
-setInterval(() => {
-  if (binanceWorker.startedSince) {
-    console.log('Binance Worker', 'Running time', moment(binanceWorker.startedSince).fromNow())
-  }
+  const bittrexWorker = new Bittrex()
+  bittrexWorker.start()
 
-  if (bittrexWorker.startedSince) {
-    console.log('Bittrex Worker', 'Running time', moment(binanceWorker.startedSince).fromNow())
-  }
-}, 5000)
+  setInterval(() => {
+    if (binanceWorker.startedSince) {
+      console.log('Binance Worker', 'Running time', moment(binanceWorker.startedSince).fromNow())
+    }
+
+    if (bittrexWorker.startedSince) {
+      console.log('Bittrex Worker', 'Running time', moment(bittrexWorker.startedSince).fromNow())
+    }
+  }, 5000)
+})

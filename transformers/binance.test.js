4929518afa0f98c245c62490ebdc7d91f4457390
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-04-14 12:38:20 +0200

    Add Bittrex transformer test

--- transformers/binance.test.js
+++ transformers/binance.test.js
@@ -1,71 +1,187 @@
 const BinanceTransformer = require('./binance')
+const ccxt = require('ccxt')
 
 describe('transformers/binance.js', () => {
 
   let transformer
+  let binanceCCXT
 
-  beforeEach(() => {
-    transformer = new BinanceTransformer()
+  beforeEach(async () => {
+    binanceCCXT = new ccxt.binance()
+
+    // Mock marketsById
+    binanceCCXT.marketsById = mockMarketsById
+
+    transformer = new BinanceTransformer(binanceCCXT)
   })
 
   it('should create a BinanceTransformer instance', () => {
     expect(transformer).toBeInstanceOf(BinanceTransformer)
   })
 
-  it('should create proper output object when giving an input object', () => {
-    expect(transformer.transformSingleObject(sampleInput)).toMatchObject({
-    'BNBBTC': {
-        'symbol': 'BNBBTC',
-        'timestamp': 123456789,
-        'datetime': '1970-01-02T10:17:36.789Z',
-        'high': 0.0025,
-        'low': 0.001,
-        'bid': 0.0024,
-        'bidVolume': 10,
-        'ask': 0.0026,
-        'askVolume': 100,
-        'vwap': undefined,
-        'open': 0.001,
-        'close': 0.0025,
-        'last': 0.0025,
-        'previousClose': 0.0009,
-        'change': 0.0015,
-        'percentage': 250,
-        'average': undefined,
-        'baseVolume': 10000,
-        'quoteVolume': 18,
-        'info': sampleInput
-      }
-    })
+  it('should output a transformed object using the CCXT data model', () => {
+    expect(transformer.transformSingleObject(sampleInput[0])).toMatchObject(expectedOutput['BNB/BTC'])
+  })
+
+  it('should ouput multiple objects when given multiple objects', () => {
+    expect(transformer.transformMultipleObjects(sampleInput)).toMatchObject(expectedOutput)
   })
 
 })
 
+const mockMarketsById = {
+  'BNBBTC': {
+    symbol: 'BNB/BTC'
+  },
+  'ETHBTC': {
+    symbol: 'ETH/BTC'
+  }
+}
 
+const sampleInput = [
+  {
+    "e": "24hrTicker",  // Event type
+    "E": 123456789,     // Event time
+    "s": "BNBBTC",      // Symbol
+    "p": "0.0015",      // Price change
+    "P": "250.00",      // Price change percent
+    "w": "0.0018",      // Weighted average price
+    "x": "0.0009",      // Previous day's close price
+    "c": "0.0025",      // Current day's close price
+    "Q": "10",          // Close trade's quantity
+    "b": "0.0024",      // Best bid price
+    "B": "10",          // Bid bid quantity
+    "a": "0.0026",      // Best ask price
+    "A": "100",         // Best ask quantity
+    "o": "0.0010",      // Open price
+    "h": "0.0025",      // High price
+    "l": "0.0010",      // Low price
+    "v": "10000",       // Total traded base asset volume
+    "q": "18",          // Total traded quote asset volume
+    "O": 0,             // Statistics open time
+    "C": 86400000,      // Statistics close time
+    "F": 0,             // First trade ID
+    "L": 18150,         // Last trade Id
+    "n": 18151          // Total number of trades
+  },
+  {
+    "e": "24hrTicker",  // Event type
+    "E": 123456789,     // Event time
+    "s": "ETHBTC",      // Symbol
+    "p": "0.0015",      // Price change
+    "P": "250.00",      // Price change percent
+    "w": "0.0018",      // Weighted average price
+    "x": "0.0009",      // Previous day's close price
+    "c": "0.0025",      // Current day's close price
+    "Q": "10",          // Close trade's quantity
+    "b": "0.0024",      // Best bid price
+    "B": "10",          // Bid bid quantity
+    "a": "0.0026",      // Best ask price
+    "A": "100",         // Best ask quantity
+    "o": "0.0010",      // Open price
+    "h": "0.0025",      // High price
+    "l": "0.0010",      // Low price
+    "v": "10000",       // Total traded base asset volume
+    "q": "18",          // Total traded quote asset volume
+    "O": 0,             // Statistics open time
+    "C": 86400000,      // Statistics close time
+    "F": 0,             // First trade ID
+    "L": 18150,         // Last trade Id
+    "n": 18151          // Total number of trades
+  }
+]
 
-
-const sampleInput = {
-  "e": "24hrTicker",  // Event type
-  "E": 123456789,     // Event time
-  "s": "BNBBTC",      // Symbol
-  "p": "0.0015",      // Price change
-  "P": "250.00",      // Price change percent
-  "w": "0.0018",      // Weighted average price
-  "x": "0.0009",      // Previous day's close price
-  "c": "0.0025",      // Current day's close price
-  "Q": "10",          // Close trade's quantity
-  "b": "0.0024",      // Best bid price
-  "B": "10",          // Bid bid quantity
-  "a": "0.0026",      // Best ask price
-  "A": "100",         // Best ask quantity
-  "o": "0.0010",      // Open price
-  "h": "0.0025",      // High price
-  "l": "0.0010",      // Low price
-  "v": "10000",       // Total traded base asset volume
-  "q": "18",          // Total traded quote asset volume
-  "O": 0,             // Statistics open time
-  "C": 86400000,      // Statistics close time
-  "F": 0,             // First trade ID
-  "L": 18150,         // Last trade Id
-  "n": 18151          // Total number of trades
+const expectedOutput = {
+  "BNB/BTC": {
+    "ask": 0.0026,
+    "askVolume": 100,
+    "average": undefined,
+    "baseVolume": 10000,
+    "bid": 0.0024,
+    "bidVolume": 10,
+    "change": 0.0015,
+    "close": 0.0025,
+    "datetime": "1970-01-02T10:17:36.789Z",
+    "high": 0.0025,
+    "info": {
+        "A": "100",
+        "B": "10",
+        "C": 86400000,
+        "E": 123456789,
+        "F": 0,
+        "L": 18150,
+        "O": 0,
+        "P": "250.00",
+        "Q": "10",
+        "a": "0.0026",
+        "b": "0.0024",
+        "c": "0.0025",
+        "e": "24hrTicker",
+        "h": "0.0025",
+        "l": "0.0010",
+        "n": 18151,
+        "o": "0.0010",
+        "p": "0.0015",
+        "q": "18",
+        "s": "BNBBTC",
+        "v": "10000",
+        "w": "0.0018",
+        "x": "0.0009"
+    },
+    "last": 0.0025,
+    "low": 0.001,
+    "open": 0.001,
+    "percentage": 250,
+    "previousClose": 0.0009,
+    "quoteVolume": 18,
+    "symbol": "BNB/BTC",
+    "timestamp": 123456789,
+    "vwap": undefined
+  },
+  "ETH/BTC": {
+    "ask": 0.0026,
+    "askVolume": 100,
+    "average": undefined,
+    "baseVolume": 10000,
+    "bid": 0.0024,
+    "bidVolume": 10,
+    "change": 0.0015,
+    "close": 0.0025,
+    "datetime": "1970-01-02T10:17:36.789Z",
+    "high": 0.0025,
+    "info": {
+        "A": "100",
+        "B": "10",
+        "C": 86400000,
+        "E": 123456789,
+        "F": 0,
+        "L": 18150,
+        "O": 0,
+        "P": "250.00",
+        "Q": "10",
+        "a": "0.0026",
+        "b": "0.0024",
+        "c": "0.0025",
+        "e": "24hrTicker",
+        "h": "0.0025",
+        "l": "0.0010",
+        "n": 18151,
+        "o": "0.0010",
+        "p": "0.0015",
+        "q": "18",
+        "s": "ETHBTC",
+        "v": "10000",
+        "w": "0.0018",
+        "x": "0.0009"
+    },
+    "last": 0.0025,
+    "low": 0.001,
+    "open": 0.001,
+    "percentage": 250,
+    "previousClose": 0.0009,
+    "quoteVolume": 18,
+    "symbol": "ETH/BTC",
+    "timestamp": 123456789,
+    "vwap": undefined
+  }
 }
---
+++ transformers/bittrex.test.js
@@ -0,0 +1,147 @@
+const BittrexTransformer = require('./bittrex')
+const ccxt = require('ccxt')
+
+describe('transformers/bittrex.js', () => {
+
+  let transformer
+  let bittrexCCXT
+
+  beforeEach(async () => {
+    bittrexCCXT = new ccxt.bittrex()
+
+    // Mock marketsById
+    bittrexCCXT.marketsById = mockMarketsById
+
+    transformer = new BittrexTransformer(bittrexCCXT)
+  })
+
+  it('should create a BittrexTransformer instance', () => {
+    expect(transformer).toBeInstanceOf(BittrexTransformer)
+  })
+
+  it('should output a transformed object using the CCXT data model', () => {
+    expect(transformer.transformSingleObject(sampleInput[0])).toMatchObject(expectedOutput['BTC/AMP'])
+  })
+
+  it('should ouput multiple objects when given multiple objects', () => {
+    expect(transformer.transformMultipleObjects(sampleInput)).toMatchObject(expectedOutput)
+  })
+
+})
+
+const mockMarketsById = {
+  'BTC-AMP': {
+    symbol: 'BTC/AMP'
+  },
+  'BTC-USDT': {
+    symbol: 'BTC/USDT'
+  }
+}
+
+const sampleInput = [
+  {
+    M: 'BTC-AMP', // symbol
+    H: 0.00003382, // high
+    L: 0.00003003, // low
+    V: 3454494.43257219, // volume (quote?)
+    l: 0.00003172, // last
+    m: 109.98346083, // base volume
+    T: 1523629402130, // timestamp
+    B: 0.00003156, // bid
+    A: 0.00003172, // ask
+    G: 393, // open buy orders
+    g: 4183, // open sell orders
+    PD: 0.00003138, // prevday
+    x: 1446578035180 // created
+  },
+  {
+    M: 'BTC-USDT', // symbol
+    H: 0.00003382, // high
+    L: 0.00003003, // low
+    V: 3454494.43257219, // volume (quote?)
+    l: 0.00003172, // last
+    m: 109.98346083, // base volume
+    T: 1523629402130, // timestamp
+    B: 0.00003156, // bid
+    A: 0.00003172, // ask
+    G: 393, // open buy orders
+    g: 4183, // open sell orders
+    PD: 0.00003138, // prevday
+    x: 1446578035180 // created
+  }
+]
+
+const expectedOutput = {
+  "BTC/AMP": {
+    "ask": 0.00003172,
+    "askVolume": undefined,
+    "average": undefined,
+    "baseVolume": 109.98346083,
+    "bid": 0.00003156,
+    "bidVolume": undefined,
+    "change": 3.4e-7,
+    "close": undefined,
+    "datetime": "2018-04-13T14:23:22.130Z",
+    "high": 0.00003382,
+    "info": {
+        "A": 0.00003172,
+        "B": 0.00003156,
+        "G": 393,
+        "H": 0.00003382,
+        "L": 0.00003003,
+        "M": "BTC-AMP",
+        "PD": 0.00003138,
+        "T": 1523629402130,
+        "V": 3454494.43257219,
+        "g": 4183,
+        "l": 0.00003172,
+        "m": 109.98346083,
+        "x": 1446578035180
+    },
+    "last": 0.00003172,
+    "low": 0.00003003,
+    "open": undefined,
+    "percentage": 1.08349267,
+    "previousClose": 0.00003138,
+    "quoteVolume": 3454494.43257219,
+    "symbol": "BTC/AMP",
+    "timestamp": 1523629402130,
+    "vwap": undefined
+  },
+  "BTC/USDT": {
+    "ask": 0.00003172,
+    "askVolume": undefined,
+    "average": undefined,
+    "baseVolume": 109.98346083,
+    "bid": 0.00003156,
+    "bidVolume": undefined,
+    "change": 3.4e-7,
+    "close": undefined,
+    "datetime": "2018-04-13T14:23:22.130Z",
+    "high": 0.00003382,
+    "info": {
+        "A": 0.00003172,
+        "B": 0.00003156,
+        "G": 393,
+        "H": 0.00003382,
+        "L": 0.00003003,
+        "M": "BTC-USDT",
+        "PD": 0.00003138,
+        "T": 1523629402130,
+        "V": 3454494.43257219,
+        "g": 4183,
+        "l": 0.00003172,
+        "m": 109.98346083,
+        "x": 1446578035180
+    },
+    "last": 0.00003172,
+    "low": 0.00003003,
+    "open": undefined,
+    "percentage": 1.08349267,
+    "previousClose": 0.00003138,
+    "quoteVolume": 3454494.43257219,
+    "symbol": "BTC/USDT",
+    "timestamp": 1523629402130,
+    "vwap": undefined
+  }
+}
